3b486b4881c89a9c8306a70322d16390e7e252f0
Justin KA <justinalexander101@gmail.com>
2024-10-21 19:45:10 -0400

    Large asteroids split in two

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_one_velocity = self.velocity.rotate(random_angle)
+        asteroid_two_velocity = self.velocity.rotate(-random_angle)
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_one.velocity = asteroid_one_velocity*1.2
+        asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_two.velocity = asteroid_two_velocity*1.2
